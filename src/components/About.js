74ad0afe2e6080ebeef25f86290ec27b86efbc55
felociouske <felociouske@gmail.com>
2026-04-25 10:09:34 +0300

    Removed the comments

--- src/components/About.js
+++ src/components/About.js
@@ -46,20 +46,6 @@ const About = () => {
               <span className="about-detail-val about-available">Open to opportunities</span>
             </div>
           </div>
-
-          {/* <a
-            href="/resume.pdf"
-            className="btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>Download Resume</span>
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4" />
-              <polyline points="7 10 12 15 17 10" />
-              <line x1="12" y1="15" x2="12" y2="3" />
-            </svg>
-          </a> */}
         </div>
 
         <div className="about-right">
